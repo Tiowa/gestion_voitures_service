3b8d6d8716ff437461569cb0c94b30522020a7b6
Tiowa <suprem3479@gmail.com>
2026-03-12 13:12:15 -0400

    Implémentation des méthodes de la classe Employe

--- main.py
+++ main.py
@@ -4,3 +4,31 @@ class Employe:
         self.nom = nom
         self.id_employe = id_employe
         self.voiture = None
+
+    def afficher_info(self):
+        if self.voiture:
+            print(f"Employé: {self.nom} - Voiture: {self.voiture.modele}")
+        else:
+            print(f"Employé: {self.nom} - Aucune voiture assignée")
+
+    def attribuer_voiture(self, voiture):
+
+        if self.voiture is not None:
+            print("Cet employé possède déjà une voiture")
+            return
+
+        if voiture.employe is not None:
+            print("Cette voiture est déjà attribuée")
+            return
+
+        self.voiture = voiture
+        voiture.employe = self
+
+    def retirer_voiture(self):
+
+        if self.voiture is None:
+            print("Cet employé n'a pas de voiture")
+            return
+
+        self.voiture.employe = None
+        self.voiture = None
